6b98d65dddf3ced62f1849fb3d704f3b94dbb26a
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2025-02-28 16:17:47 -0300

    Start reading from the end of the file
    xfce4-clipman saves the last copied text at the end of the file. To
    display the most recent copied text at the top in rofi, I need to read
    the file from the end.

--- read_clipman.c
+++ read_clipman.c
@@ -3,47 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void) {
-  const char *home = getenv("HOME");
-  if (!home) {
-    fprintf(stderr, "Unable to get the HOME path");
-    return 1;
-  }
-  const char *file = "/.cache/xfce4/clipman/textsrc"; // From home
-
-  char *path = malloc(strlen(home) + strlen(file) + 1);
-  strcpy(path, home);
-  strcat(path, file);
-
-  FILE *f = fopen(path, "r");
-  if (!f) {
-    fprintf(stderr, "Unable to open '%s'", path);
-    free(path);
-    return 1;
-  }
-  free(path);
-
-  fseek(f, 14, SEEK_SET); // skip: "[texts]\ntexts="
-  // I have to set a variable "new_element" because xfce4-clipman puts
-  // ';' at the end.
-  bool escaped = false, new_element = false;
+bool print_line(FILE *f) {
   char c;
-  while ( (c = fgetc(f)) != EOF && c != '\n') {
-    if (new_element) {
-      putchar('\n');
-      new_element = false;
+  int move = 1;
+  bool escaped = false;
+
+  while ( (c = fgetc(f)) != ';' || escaped) {
+    if (c == EOF) {
+      fprintf(stderr, "File format error! EOF reached\n");
+      return true;
     }
 
     switch (c) {
       case '\\':
         if (escaped) printf("\\\\");
         escaped = !escaped;
-        break;
-
-      case ';':
-        if (escaped) putchar(c);
-        else new_element = true;
-        escaped = false;
         break;
 
       case 's':
@@ -53,6 +27,11 @@ int main(void) {
 
       case 't':
         putchar( (escaped) ? '\t' : c );
+        escaped = false;
+        break;
+
+      case ';':
+        putchar(';');
         escaped = false;
         break;
 
@@ -68,14 +47,73 @@ int main(void) {
       default:
         if (escaped) {
           fprintf(stderr, "Error parsing the clipboard!. Unknown escape sequence: \\%c\n", c);
-          fclose(f);
-          return 1;
+          return false;
         }
         putchar(c);
         escaped = false;
         break;
     }
+
+    move++;
   }
+  putchar('\n');
+  fseek(f, -1 * move, SEEK_CUR);
+
+  return true;
+}
+
+int main(void) {
+  const char *home = getenv("HOME");
+  if (!home) {
+    fprintf(stderr, "Unable to get the HOME path\n");
+    return 1;
+  }
+  const char *file = "/.cache/xfce4/clipman/textsrc"; // From home
+
+  char *path = malloc(strlen(home) + strlen(file) + 1);
+  strcpy(path, home);
+  strcat(path, file);
+
+  FILE *f = fopen(path, "r");
+  if (!f) {
+    fprintf(stderr, "Unable to open '%s'\n", path);
+    free(path);
+    return 1;
+  }
+
+  // '-4': skip: last semicolon and 2 break line (one from xfce, other for
+  //       POSIX) and position the cursor in the last char of the sentence. See also:
+  //       https://stackoverflow.com/questions/729692/why-should-text-files-end-with-a-newline
+  // '14': skip: "[texts]\ntexts="
+  int pos;
+  if ( fseek(f, -4, SEEK_END) || (pos = ftell(f)) < 14 ) {
+    fprintf(stderr, "File format error! File: %s\n", path);
+    free(path);
+    return 1;
+  }
+  free(path);
+
+  char c;
+  while (pos > 14) {
+    if ( (c = fgetc(f)) == ';' ) {
+      int x = 1;
+      bool escaped = false;
+      while ( !fseek(f, -2, SEEK_CUR) && (c = fgetc(f)) == '\\' ) {
+        escaped = !escaped;
+        x++;
+      }
+      fseek(f, x, SEEK_CUR);
+
+      if (!escaped && !print_line(f)) {
+        fclose(f);
+        return 1;
+      }
+    }
+
+    fseek(f, -2, SEEK_CUR);
+    pos--;
+  }
+  print_line(f);
 
   fclose(f);
   return 0;
